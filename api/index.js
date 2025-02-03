a6a085974fc837ca8c9883c0dd930844eac8fe39
david <dahodprod@gmail.com>
2025-02-03 13:44:30 +0100

    npm install & api

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import axios from 'axios';
+
+dotenv.config({ path: '../.env' });
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/pokemon/:name', async (req, res) => {
+    const { name } = req.params;
+    try {
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des données' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
