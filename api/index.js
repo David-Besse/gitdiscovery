25a8cde5bd8b2152392381d08017ef404b655921
david <dahodprod@gmail.com>
2025-02-03 14:52:03 +0100

    fix: relative path for dotenv

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import pokemonRoutes from './routes/index.js';
 
-dotenv.config({ path: '../.env' });
+dotenv.config({ path: './.env' });
 
 const app = express();
 const PORT = process.env.PORT;
