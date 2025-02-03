b2146d1c3c278473285e74cc0a2eafe352d875d8
TheYassir <arasyassir@gmail.com>
2025-02-03 16:37:42 +0100

    📦 NEW: Test no concluant

--- api/test/getByName.test.js
+++ api/test/getByName.test.js
@@ -1,10 +1,15 @@
-import { getByName } from "../utils/getByName.js";
+// import { getByName } from "../utils/getByName.js";
+const { getByName } = require("./utils/getByName");
 
-test("the fetch fails with an error if name does'nt exist", async () => {
-  expect.assertions(1);
-  try {
-    await getByName("Bloublou");
-  } catch (error) {
-    expect(error).toMatch("error");
-  }
+// test("the fetch fails with an error if name does'nt exist", async () => {
+//   // expect.assertions(1);
+//   try {
+//     await getByName("Bloublou");
+//   } catch (error) {
+//     expect(error).toMatch("error");
+//   }
+// });
+
+test("la récupération échoue avec une erreur si pokemon name existe pas", async () => {
+  await expect(getByName("LOULOU")).rejects.toMatch("error");
 });
--- api/test/getByNumber.test.js
+++ api/test/getByNumber.test.js
@@ -1,10 +1,15 @@
-import { getByNumber } from "../utils/getByNumber.js";
+// import { getByNumber } from "../utils/getByNumber.js";
+const { getByNumber } = require("./utils/getByNumber");
 
-test("the fetch fails with an error if number is not match pokemon", async () => {
-  expect.assertions(1);
-  try {
-    await getByNumber(18938839);
-  } catch (error) {
-    expect(error).toMatch("error");
-  }
+// test("the fetch fails with an error if number is not match pokemon", async () => {
+//   expect.assertions(1);
+//   try {
+//     await getByNumber(18938839);
+//   } catch (error) {
+//     expect(error).toMatch("error");
+//   }
+// });
+
+test("la récupération échoue avec une erreur si pokemon id existe pas", async () => {
+  await expect(getByNumber(27002020202)).rejects.toMatch("error");
 });
