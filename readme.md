5c330ccaccce5bce6ad2ab76efd5561779bf28bd
MehdiSDK <mehdi@sudokeys.com>
2025-02-03 15:26:32 +0100

    👌 IMPROVE: ajout dans le readme

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 ## Laisse moi les droits DAVID
 
 **_Test_**
+
+**Bonjour je m'appelle Mehdi**
--- test.js
+++
@@ -1,3 +0,0 @@
-import * as api from './api/index.js';
-
-api.get(); // Serveur en cours d'exécution sur le port 3000
